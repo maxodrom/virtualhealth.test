c0fa6ce16de49447f69f59121f2e7e906fe8bba5
Maxodrom <max@maxodrom.ru>
2016-04-10 01:01:46 +0300

    Index view prepared.

--- www/protected/views/site/index.php
+++ www/protected/views/site/index.php
@@ -1,20 +1,32 @@
 <?php
-/* @var $this SiteController */
+/** @var $this SiteController */
+/** @var $dataProvider CActiveDataProvider|CSqlDataProvider */
 
-$this->pageTitle=Yii::app()->name;
+/** @var $clientScript CClientScript */
+$clientScript = Yii::app()->clientScript;
+$clientScript->registerCss(
+    'pager',
+    'ul.yiiPager .first, ul.yiiPager .last {display:inline !important;}'
+);
+
+$this->pageTitle = Yii::app()->name;
 ?>
+<div class="row">
+    <?php if ($dataProvider instanceof CActiveDataProvider): ?>
+        <h2>Решение с помощью AR-модели</h2>
+    <?php else: ?>
+        <h2>Решение с помощью DAO</h2>
+    <?php endif; ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+    <?php $this->widget('zii.widgets.grid.CGridView', [
+        'dataProvider' => $dataProvider,
+        'enableHistory' => true,
+        //'ajaxUpdate'=>false,
+        'columns' => [
+            'cx:text:CX',
+            'rx:text:RX',
+            'title:text:Title',
+            'ndc:text:NDC',
+        ]
+    ]); ?>
+</div>
