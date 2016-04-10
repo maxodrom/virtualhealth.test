188e46be6f972a7415323674ac88e13aa1d3cc1f
Maxodrom <max@maxodrom.ru>
2016-04-10 13:44:07 +0300

    Some improvements.

--- www/protected/migrations/m160409_222243_init.php
+++ www/protected/migrations/m160409_222243_init.php
@@ -4,6 +4,8 @@ class m160409_222243_init extends CDbMigration
 {
 	public function safeUp()
 	{
+        set_time_limit(0);
+        ignore_user_abort(true);
 		$this->execute(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'db.sql'));
 	}
 
